f527c751039496b48bd7b0d50dd9762abfe3340f
Kurper <kurper@gmail.com>
2011-01-16 23:40:22 -0500

    grass

--- src/mobster.py
+++ src/mobster.py
@@ -2,11 +2,23 @@
 
 import sys, pygame
 
+def background(screen, picture):
+    rect = picture.get_rect()
+    rows = 480//rect.height + 1
+    cols = 640//rect.width + 1
+    for x in range(cols):
+        for y in range(rows):
+            rect.topleft = (x*rect.width, y*rect.height)
+            screen.blit(picture, rect)
+
+
+
 if __name__ == "__main__":
     pygame.init()
 
     screen = pygame.display.set_mode((640, 480))
     guy = pygame.image.load('data/dude.png').convert()
+    grass = pygame.image.load('data/grass.png').convert()
     pos = guy.get_rect()
     pos = pos.move((200, 0))
     pos = pos.move((0, 50))
@@ -15,6 +27,6 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        screen.fill((0,0,0))
+        background(screen, grass)
         screen.blit(guy, pos)
         pygame.display.flip()
