853cc7c9716b97defff1baa8bde1efa9eea842f3
Kurper <kurper@gmail.com>
2011-01-17 15:29:12 -0500

    added an enemy!

--- src/mobster.py
+++ src/mobster.py
@@ -5,48 +5,51 @@ from pygame.locals import *
 from sprites import load_png
 
 class Grid:
-    def __init__(self, screen, background_filename):
+    def __init__(self, background_filename, cols, rows):
         '''Creates a grid, with size set by the background's size.'''
         picture, rect = load_png(background_filename)
         self.screen = screen
         self.guys = []
         self.col_width = rect.width
         self.row_height = rect.height
+        self.cols = cols
+        self.rows = rows
 
-        width = screen.get_width()
-        height = screen.get_height()
-        self.background = pygame.Surface((width, height))
+        self.background = pygame.Surface((self.col_width * cols,
+                                          self.row_height * rows))
 
-        cols = width//rect.width + 1
-        rows = height//rect.height + 1
-
-        for x in range(cols):
-            for y in range(rows):
+        for x in range(self.cols):
+            for y in range(self.rows):
                 rect.topleft = (x*rect.width, y*rect.height)
                 self.background.blit(picture, rect)
 
-    def draw(self):
-        self.screen.blit(self.background, (0,0))
+    def draw(self, screen):
+        screen.blit(self.background, (0,0))
         for guy in self.guys:
-            self.screen.blit(guy.image, (guy.x * self.col_width,
-                                         guy.y * self.row_height))
+            screen.blit(guy.image, (guy.x * self.col_width,
+                                    guy.y * self.row_height))
 
     def add(self, guy):
         self.guys.append(guy)
 
 class Guy:
-    def __init__(self, filename):
-        self.x = 3
-        self.y = 1
+    def __init__(self, filename, x, y):
+        self.x = x
+        self.y = y
         self.image, _ = load_png(filename)
 
 if __name__ == "__main__":
     pygame.init()
 
     screen = pygame.display.set_mode((640, 480))
-    grid = Grid(screen, 'grass')
-    guy = Guy('dude')
+
+    guy = Guy('dude', 2, 4)
+    enemy = Guy('dude', 8, 4)
+    enemy.image = pygame.transform.flip(enemy.image, True, False)
+
+    grid = Grid('grass', 10, 6)
     grid.add(guy)
+    grid.add(enemy)
     while 1:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -61,5 +64,5 @@ if __name__ == "__main__":
                 elif event.key == K_RIGHT:
                     guy.x += 1
                     
-        grid.draw()
+        grid.draw(screen)
         pygame.display.flip()
